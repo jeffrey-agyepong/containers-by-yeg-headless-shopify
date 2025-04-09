79443b7c13ff7e0bc5c117751a584674a0b01f9b
Jeffrey A <agyepong99@gmail.com>
2025-04-08 22:51:38 -0600

    first

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify"; 
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  output: "static",
+  adapter: netlify(),
+
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    define: {
+      'process.env': process.env
+    }
+  },
+});
